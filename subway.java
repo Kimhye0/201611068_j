902c5916fa2029610628e3cc3900ea51a6d0c709
201611068 김혜원 <akrnrh3737@naver.com>
2016-09-20 23:37:50 +0900

    subway

---
+++ subway.java
@@ -0,0 +1,12 @@
+class Subway{
+	int line;
+	void run(){
+		System.out.println(line+"line runs");
+	}
+	void setLine(int 1){
+		line=1;
+	}
+}
+Subway mySub=new Subway();
+mySub.setLine(3)
+mySub.run()
